44c1eb12fe1f5db5834ad23a78668511421fd408
Karla Fernanda Dellarett de Sá <karlavet86@gmail.com>
2022-05-09 17:14:33 -0300

    novos arquivos da aula de javascript

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,63 @@
+/* Resposta dos desafios
+1. 10 e 10, 5
+2. 10
+3. let p = suaCargaHorariaTrabalhadaDia
+let t = valorRecebidoDiaTrabalhado
+
+*/
+
+prompt("Qual é o seu nome?")
+prompt("Qual é a sua idade?")
+
+// let = prompt("Qual é o seu nome?")
+// let = prompt("Qual é a sua idade?")*/
+
+
+const suaIdade = prompt("Qual é a sua idade?")
+console.log(suaIdade)
+
+const seuNome = prompt("Qual é o seu nome?")
+console.log(seuNome)
+
+console.log("Olá", "seu nome é", seuNome, "e você tem", suaIdade,"anos")
+
+const pergunta1 = prompt("Você está de calça comprida?")
+console.log("Você está de calça comprida", pergunta1)
+
+
+
+
+const pergunta2 = prompt("Você sabe onde mora?")
+console.log(pergunta2)
+
+const pergunta3 = prompt("Você tem carro?")
+console.log(pergunta3)
+
+let a = 10
+let b = 25 
+let c = null
+c = a
+a = b
+b = c
+console.log("O novo valor de a é",a )
+console.log("O novo valor de b é", b)
+
+
+// desafio
+
+let numero1 = prompt("Digite um número")
+let numero1 = 
+let numero2 =prompt("Digite outro número")
+
+console.log(typeof )
+// console.log(typeof seuNome)
+// console.log(typeof suaIdade)
+
+
+// apareceu uma tela de erro no console. Aconteceu porque não atribui nenhum valor a variável
+
+
+
+
+
+
