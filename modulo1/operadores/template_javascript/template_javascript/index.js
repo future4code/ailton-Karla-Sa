43020ffc15099d5d70a9c4b75d1d933fe0398cac
Karla Fernanda Dellarett de Sá <karlavet86@gmail.com>
2022-05-10 14:51:16 -0300

    exercícios da aula de lógica

---
+++ modulo1/operadores/template_javascript/template_javascript/index.js
@@ -0,0 +1,62 @@
+// exercício de interpretação de código
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2 
+
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado)
+// // false
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado) 
+// // false
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado)
+// // true
+
+// console.log("d. ", typeof resultado)
+// // boolean
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+// // faltou ele colocar o number porque todo prompt retorna como string
+
+// let idade = prompt("Digite a sua idade")
+// let idadeMelhorAmigo = prompt("Digite a idade do seu melhor amigo ou da sua melhor amiga")
+// let resultadoIdade = (idade > idadeMelhorAmigo) 
+// console.log("Sua idade é maior do que a do seu melhor amigo?", resultadoIdade)
+
+// let diferencaDeIdade = Number(idade - idadeMelhorAmigo)
+
+// console.log("A diferença de idade entre vocês é de", diferencaDeIdade, "anos")
+
+// let numeroPar = Number(prompt("Digite um número par"))
+// let divisor = 2
+// let divisao = (numeroPar % divisor)
+// console.log("O resultado da divisão do número é", divisao)
+// o resto é sempre zero
+// o resto é sempre 1, quando o usuário insere um número ímpar
+
+// let idade = Number(prompt("Digite a sua idade"))
+// let idadeEmMeses = idade * 12
+// let idadeEmDias = idade * 365
+// let idadeEmHoras = idadeEmDias * 24
+// console.log("Sua idade em meses", idadeEmMeses, "em dias", idadeEmDias, "e em horas", idadeEmHoras)
+
+let numero1 = prompt("Insira um número")
+let numero2 = prompt("Insira outro número")
+let resultado1 = numero1 > numero2
+console.log(resultado1)
+let resultado2 = numero1 === numero2
+console.log(resultado2)
+let resultado3 = numero1 / numero2
+let resultado = resultado3 === 0
+console.log(resultado)
+let resultado4 = (numero2 / numero1) === 0
+console.log(resultado4)
+
+
+// console.log(resultado1, resultado2, resultado3, resultado4)
---
+++ modulo1/projeto-readme/aprendizados.md
@@ -0,0 +1,2 @@
+# Aprendizados da semana
+Nessa primeira semana do curso na **Labenu** aprendi a mexer no terminal, no VsCode e a ficar maluca com o git.
