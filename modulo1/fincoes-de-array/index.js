c1944c605b3d767d4aec0399dd3cd3996c83080a
Karla Fernanda Dellarett de Sá <karlavet86@gmail.com>
2022-05-19 20:30:26 -0300

    exercícios de funcoes de array

---
+++ modulo1/fincoes-de-array/index.js
@@ -0,0 +1,138 @@
+// Exercícios de interpretação de código
+// // 1. todos os nomes e seus respectivos apelidos
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" }
+//   ]
+  
+//   const novoArrayA = usuarios.map((item, index, array) => {
+//      console.log(item, index, array)
+//   })
+// // 2. aparecerá somente os nomes
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayB = usuarios.map((item, index, array) => {
+//      return item.nome
+//   })
+  
+//   console.log(novoArrayB)
+
+// 3. filtrará somente tudo menos o que tiver escrito "Chijo"
+
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayC = usuarios.filter((item, index, array) => {
+//      return item.apelido !== "Chijo"
+//   })
+  
+//   console.log(novoArrayC)
+
+// Exercícios de escrita de código
+// 1) a) a) Crie um novo array que contenha apenas o nome dos doguinhos
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+//  const novoArray = pets.map((item, array) => {
+//     return item.nome
+//  })
+
+//  console.log(novoArray)
+
+// //  b)Crie um novo array apenas com os cachorros salsicha
+
+// const arrayB = pets.filter((item, index) => {
+//     return item.raca === "Salsicha"
+// })
+
+// console.log(arrayB)
+
+// c) Crie um novo array que possuirá mensagens para enviar para todos os clientes que são poodles.
+//  A mensagem deve ser: "Você ganhou um cupom de desconto de 10% para tosar o/a [NOME]!"
+
+// const arrayC = pets.filter((item, index) =>{
+//     if (item.raca === "Poodle"){
+//         console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${item.nome}!`)
+//         } else {
+//             console.log(`Infelizmente seu pet ${item.nome} não ganhou uma tosa`)
+//         }
+// })
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+//  a) Crie um novo array que contenha apenas o nome de cada item
+
+// const novoArray = produtos.map((item) => {
+//     return item.nome
+// }
+// )
+// console.log(novoArray)
+
+// b) Crie um novo array que contenha um objeto com o nome e o preço de cada item, 
+// aplicando 5% de desconto em todos eles
+
+const arrayB = produtos.map((item, index) =>{
+    return { nome: item.nome, preco: item.preco * 0.95}
+
+}
+)
+console.log(arrayB)
+
+// c) Crie um novo array que contenha apenas os objetos da categoria Bebidas
+
+const retornoBebidas = produtos.filter((item) => {
+    return item.categoria === "Bebidas"
+})
+
+console.log(retornoBebidas)
+
+// 
+// d) Crie um novo array que contenha apenas os objetos cujo nome contenha a palavra "Ypê" (não é uma parte do objeto)
+
+const retornoYpe = produtos.filter((item) =>{
+    return item.nome.includes("Ypê")
+})
+
+console.log(retornoYpe)
+
+// e) Crie um novo array onde cada item é uma frase "Compre [NOME] por [PREÇO]". 
+// Esse array deve conter frases apenas dos itens cujo nome contenha a palavra "Ypê"
+
+const novoArray = produtos.map((item) => {
+    return (`Compre ${item.nome} por ${item.preco}`)
+    
+
+})
+
+console.log(novoArray)
+
+const somenteYpe = produtos.filter((item) => {
+    return item.nome.includes("Ypê")
+})
+
+console.log(somenteYpe)
